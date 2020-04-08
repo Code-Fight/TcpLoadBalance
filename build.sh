4c71bae20abde37286b95afd77469583fe16b1e6
zfCode <zfcode@qq.com>
2020-04-08 16:32:51 +0800

    add build version and load balance algorithm

---
+++ build.sh
@@ -0,0 +1,42 @@
+#当前版本号,每次更新服务时都必须更新版本号
+CurrentVersion=0.0.3
+#项目名
+Project=TcpLoadBalance
+
+#编译目标平台 (windows,linux...)
+targetGOOS=windows
+targetGOARCH=amd64
+
+#二进制生成文件存放路径
+binPath=/Users/zhangfeng/Documents/GoProject/Bin/
+
+
+
+#######################以上为配置项#######################
+
+
+
+
+GitCommit=$(git rev-parse --short HEAD || echo unsupported)
+
+fileName=""
+if (($targetGOOS == windows))
+then
+  fileName=$Project".exe"
+fi
+
+GOOS=$targetGOOS GOARCH=$targetGOARCH go build -o $binPath'/'$Project'/'$fileName  -ldflags "-X main.Version=$CurrentVersion -X 'main.BuildTime=`date "+%Y-%m-%d %H:%M:%S"`' -X 'main.GoVersion=`go version`' -X main.GitCommit=$GitCommit"
+
+if [ $? -eq 0 ]; then
+  echo "##############################"
+  echo "build succeed !!"
+  echo "Version:" $CurrentVersion
+  echo "Git commit:" $GitCommit
+  echo "Go version:" `go version`
+  echo "##############################"
+else
+  echo "##############################"
+  echo "ERROR!"
+  echo "##############################"
+fi
+
